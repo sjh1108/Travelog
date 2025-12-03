739e670cbf251fae577c61f3c9cf9098ebbbec49
sjh1108 <ch6854@naver.com>
2025-12-03 15:44:12 +0900

    [FIX] tailwind 에러 해결

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -2,14 +2,10 @@
 export default {
   content: [
     "./index.html",
-    "./src/**/*.{vue,js,jsx,vue,ts,tsx}",
+    "./src/**/*.{vue,js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {
-      colors: {
-        background: "#ffffff", // 원하는 배경색 코드 (예: 흰색)
-        // 만약 'foreground' 같은 다른 에러도 난다면 여기에 추가
-      },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
